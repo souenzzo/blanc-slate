1d912b5fb759d45af822118ca2f2c49f8ead1a4c
Ingrid <ingridespangler@gmail.com>
2019-02-06 22:23:44 -0200

    melhorando a pagina de admin

--- schedule/admin.py
+++ schedule/admin.py
@@ -3,6 +3,11 @@ from .models import Aluna, Professora, Horario, Modalidade, Sala
 # Register your models here.
 admin.site.register(Aluna)
 admin.site.register(Professora)
-admin.site.register(Horario)
-admin.site.register(Modalidade)
+@admin.register(Horario)
+class HorarioAdmin(admin.ModelAdmin):
+    list_filter = ('dia_da_semana', 'horario')
+@admin.register(Modalidade)
+class ModalidadeAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'sala')
+    list_filter = ('sala',)
 admin.site.register(Sala)
