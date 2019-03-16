d5191a0f00e4fe22c4a7aa01427eb7c4dc343462
Enzzo Cavallo <souenzzo@gmail.com>
2019-03-16 18:27:13 -0300

    +bokeh sample

--- schedule/urls.py
+++ schedule/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [ path('',views.index, name='index'),]
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('bokeh_demo', views.bokeh_demo, name='bokeh_demo'),
+]
--- schedule/views.py
+++ schedule/views.py
@@ -1,7 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
+
+from bokeh.plotting import figure, output_file, show
+from bokeh.embed import components
+
+
 
 # Create your views here.
 
 def index(request):
     return HttpResponse("Olá, esta é a sua tabela de horários.")
+
+def bokeh_demo(request):
+    x= [1,3,5,7,9,11,13]
+    y= [1,2,3,4,5,6,7]
+    title = 'y = f(x)'
+
+    plot = figure(title= title ,
+                  x_axis_label= 'X-Axis',
+                  y_axis_label= 'Y-Axis',
+                  plot_width =400,
+                  plot_height =400)
+
+    plot.line(x, y, legend= 'f(x)', line_width = 2)
+    #Store components
+    script, div = components(plot)
+
+    #Feed them to the Django template.
+    return render_to_response( 'bokeh/demo.html',
+                               {'script' : script , 'div' : div} )
